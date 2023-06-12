92614ecb01cb181dabe34c5e782dcb2e090b38d9
Evan Krambuhl <evan.krambuhl@gmail.com>
2023-06-12 00:43:06 -0700

    vite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 import solid from 'solid-start/vite';
 import vercel from 'solid-start-vercel';
-import solidStyled from 'vite-plugin-solid-styled';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
@@ -9,13 +8,6 @@ export default defineConfig({
     tsconfigPaths(),
     solid({
       adapter: vercel(),
-    }),
-    solidStyled({
-      verbose: true,
-      filter: {
-        include: 'src/**/*.{ts,js,tsx,jsx}',
-        exclude: 'node_modules/**/*.{ts,js,tsx,jsx}',
-      },
     }),
   ],
 });
